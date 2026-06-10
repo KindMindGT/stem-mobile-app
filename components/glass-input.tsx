3d9b6c6bece8b0c5b43eb43044be8128eea2f7f7
Emmanuel Alvarado <josue0ghost@gmail.com>
2026-06-10 05:34:50 -0600

    Fix glass-input font reference
    components/glass-input.tsx(61,17): error TS2304: Cannot find name 'FONTS'.

--- components/glass-input.tsx
+++ components/glass-input.tsx
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
   },
   input: {
     marginTop: 2,
-    fontFamily: FONTS.interSemiBold,
+    //fontFamily: FONTS.interSemiBold,
     //fontWeight: '600',
     fontSize: 16,
     color: '#fff',
